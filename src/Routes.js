30710d9cf7f9b759e8592e2a289b46fabc495514
Shubi-7 <45991712+Shubi-7@users.noreply.github.com>
2021-04-11 08:56:53 +0545

    Reset Password Added!!

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Forgetpassword from './components/auth/Forgetpassword'
+import Resetpassword from './components/auth/Resetpassword'
 import Signin from './components/auth/Signin'
 import Signup from './components/auth/Signup'
 import Homepage from './components/pages/Homepage'
@@ -14,6 +15,8 @@ const Routes=()=> {
                 <Route exact path="/signup" component={Signup}/>
                 <Route exact path="/signin" component={Signin}/>
                 <Route exact path="/forget/password" component={Forgetpassword}/>
+                <Route exact path="/reset/password" component={Resetpassword}/>
+
             </Switch>
         </Router>
 
--- src/components/auth/Resetpassword.js
+++ src/components/auth/Resetpassword.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from '../layouts/Navbar'
 
 const Resetpassword = () => {
     return (
-        <div>
-            
+        <>
+        <Navbar/>
+        <br/>
+
+        <div className="banner-top">
+            <div className="container">
+                <h3 >Reset Password</h3>
+                <h4><Link to="/">Home</Link><label>/</label>ResetPassword</h4>
+                <div className="clearfix"> </div>
+            </div>
         </div>
+
+            <div className="login">
+            
+                <div className="main-agileits">
+                        <div className="form-w3agile">
+                            <h3>Reset Password</h3>
+                            <form action="#" method="post">
+                                <div className="key">
+                                    <i className="fa fa-envelope" aria-hidden="true"></i>
+                                    <input  type="text"  name="Email"  required=""/>
+                                    <div className="clearfix"></div>
+                                </div>
+                                <div className="key">
+                                    <i className="fa fa-lock" aria-hidden="true"></i>
+                                    <input  type="password"  name="Password" required=""/>
+                                    <div className="clearfix"></div>
+                                </div>
+                                <button className="btn btn-warning">Change Password</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            
+        </>
     )
 }
 
